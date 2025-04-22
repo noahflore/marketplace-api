260e6bc6b916bcd5700c8d39df82674fa1551b5e
giovanne <giovannepm@gmail.com>
2025-04-21 23:35:52 -0300

    fit: createService for User and router

---
+++ src/modules/Users/useCases/create/createService.ts
@@ -0,0 +1,7 @@
+import { User } from "modules/Users/entities/User";
+
+export class CreateService{
+    public execute(body: User){
+
+    }
+}
--- src/serve.ts
+++ src/serve.ts
@@ -1,11 +1,14 @@
 import Express , {json} from 'express'
 import cors from 'cors'
 import connectToMongoDB from 'database';
+import router from 'Routers';
 
 const app = Express();
 app.use(json())
 app.use(cors())
 connectToMongoDB.execute();
+
+app.use(router)
 
 const port = process.env.PORT || 5000
 
