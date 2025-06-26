08097d75afe29bb6f982b885ab46672f2278971c
giovanne <giovannepm@gmail.com>
2025-06-26 16:58:39 -0300

    fix: required without "( )"

--- src/modules/Users/schemas/joi/AddressSchemaJoi.ts
+++ src/modules/Users/schemas/joi/AddressSchemaJoi.ts
@@ -3,7 +3,7 @@ import joi from "joi"
 export const AddressSchemaJoi = joi.object({
     
     street: joi.string().required(),
-    number: joi.number().required,
+    number: joi.number().required(),
     complement: joi.string(),
     zipcode: joi.string().required(),
     createdAt: joi.date()
