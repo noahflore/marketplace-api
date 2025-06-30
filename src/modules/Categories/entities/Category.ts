eeab06386c5a65befe54c48387868587090bbd5d
giovanne <giovannepm@gmail.com>
2025-06-30 18:40:36 -0300

    fix: Category sign with userId

--- src/modules/Categories/entities/Category.ts
+++ src/modules/Categories/entities/Category.ts
@@ -3,11 +3,13 @@ import { ObjectId } from "mongodb"
 export class Category{
     public _id: ObjectId
     public name: string
+    public IDuser:string
     public created_at: Date
 
-    constructor (_id: ObjectId, name: string, created_at: Date){
+    constructor (_id: ObjectId, name: string, IDUser: string, created_at: Date){
         this._id= _id
         this.name= name
+        this.IDuser= IDUser
         this.created_at= created_at
     }
 }
