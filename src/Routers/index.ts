3ac9c29677089151041f4e1c16af3f209dbad787
giovanne <giovannepm@gmail.com>
2025-07-01 02:08:47 -0300

    fit: router categories

--- src/Routers/index.ts
+++ src/Routers/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouters from "./Users/userRoutes";
 import authRouter from "./Auth/authRouters";
+import categoryRouters from "./Categories/CategoryRoutes";
 
 const router = Router()
 
 router.use("/users", userRouters)
 router.use("/auth", authRouter)
+router.use("/categories", categoryRouters)
 
 export default router
