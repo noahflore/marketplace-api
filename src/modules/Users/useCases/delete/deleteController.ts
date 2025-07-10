a21ae9b7ad8c575cc8b5b51449704c9a37117b6b
giovanne <giovannepm@gmail.com>
2025-07-10 06:53:48 -0300

    fix: const data removed of deleteController

--- src/modules/Users/useCases/delete/deleteController.ts
+++ src/modules/Users/useCases/delete/deleteController.ts
@@ -6,7 +6,6 @@ class DeleteController{
     async handle(req: Request, res: Response): Promise<Response>{
         try {
             const { _id } = res.locals.user
-            const data = req.body
             const deleteService = container.resolve(DeleteService)
             await deleteService.execute(_id)
             return res.sendStatus(204)
