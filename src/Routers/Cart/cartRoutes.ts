c5e9672317729966ef33f7038699d8ac12056b22
giovanne <giovannepm@gmail.com>
2025-07-18 17:25:21 -0300

    fit: update and delete cart of router implementated

--- src/Routers/Cart/cartRoutes.ts
+++ src/Routers/Cart/cartRoutes.ts
@@ -2,8 +2,10 @@ import { Router } from "express"
 import authMiddleware from "middleware/authMiddleware"
 import paginationMiddleware from "middleware/paginationMiddleware"
 import createController from "modules/Carts/useCases/create/createController"
+import deleteController from "modules/Carts/useCases/delete/deleteController"
 import findAllController from "modules/Carts/useCases/findAll/findAllController"
 import findByIdController from "modules/Carts/useCases/findById/findByIdController"
+import updateController from "modules/Carts/useCases/update/updateController"
 
 const cartRouters = Router()
 
@@ -11,5 +13,7 @@ cartRouters.use(authMiddleware.execute)
 cartRouters.post("/", createController.handle)
 cartRouters.get("/", paginationMiddleware.execute, findAllController.handle)
 cartRouters.get("/:id", findByIdController.handle)
+cartRouters.patch("/:id", updateController.handle)
+cartRouters.delete("/:id", deleteController.handle)
 
 export default cartRouters
