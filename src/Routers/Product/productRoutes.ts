bfb82cb2bc156fce3bb34c92ee3e98109c0d621c
giovanne <giovannepm@gmail.com>
2025-07-21 01:36:54 -0300

    fit: find all router product

--- src/Routers/Product/productRoutes.ts
+++ src/Routers/Product/productRoutes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express"
 import authMiddleware from "middleware/authMiddleware"
+import paginationMiddleware from "middleware/paginationMiddleware"
 import createController from "modules/Products/useCases/create/createController"
+import findAllController from "modules/Products/useCases/findAll/findAllController"
 
 
 const productRouters = Router()
 
 productRouters.use(authMiddleware.execute)
 productRouters.post("/", createController.handle)
+productRouters.get("/", paginationMiddleware.execute, findAllController.handle)
 
 export default productRouters
