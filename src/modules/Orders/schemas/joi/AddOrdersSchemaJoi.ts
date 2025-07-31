e92e1be096048b9abf529f8c4356e0a78fef444c
giovanne <giovannepm@gmail.com>
2025-07-31 12:34:05 -0300

    fix: schema joi order have only a file with validation

--- src/modules/Orders/schemas/joi/AddOrdersSchemaJoi.ts
+++
@@ -1,6 +0,0 @@
-import joi from "joi"
-
-export const AddOrdersSchemaJoi = joi.object({
-    ready: joi.boolean().default(false),
-    created_at: joi.date()
-})
--- src/modules/Orders/schemas/joi/OrderSchemaJoi.ts
+++ src/modules/Orders/schemas/joi/OrderSchemaJoi.ts
@@ -1,5 +1,11 @@
 import joi from "joi"
 
 export const OrderSchemaJoi = joi.object({
-    add_orders: joi.array()
-})
+  add_orders: joi.array().items(
+    joi.object({
+      _id: joi.string().length(24).hex().required(),
+      ready: joi.boolean().default(false),
+      created_at: joi.date()
+    })
+  )
+});
