e756e231c90ae6d087db52af93ff6f972de68dea
giovanne <giovannepm@gmail.com>
2025-07-31 12:47:32 -0300

    fix: schema joi cart has only a file

--- src/modules/Carts/schemas/joi/CartSchemaJoi.ts
+++ src/modules/Carts/schemas/joi/CartSchemaJoi.ts
@@ -1,6 +1,14 @@
-import joi from "joi"
+import joi from "joi";
 
 export const CartSchemaJoi = joi.object({
-    add_product: joi.array(),
-    created_at: joi.date()
-})
+  add_products: joi.array().items(
+    joi.object({
+      _id: joi.string().length(24).hex().required(),
+      userId: joi.string().length(24).hex().required(),
+      amount: joi.number().min(1).required(),
+      price_unit: joi.number().min(0).required(),
+      freight: joi.number().min(0).required(),
+      created_at: joi.date()
+    })
+  )
+});
